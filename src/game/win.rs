249f01bfecceeb2e4232a8a8b41c98c98307ae3f
mylastresort <samytamim0@gmail.com>
2026-01-09 13:29:15 +0100

    refactor: check_win in Win trait

--- src/game/win.rs
+++ src/game/win.rs
@@ -17,40 +17,58 @@ pub struct Win {
 pub trait GameWin {}
 
 impl Win {
+    const CAPTURES_TO_WIN: usize = 5;
+
     pub fn check_for_win(
-        _game_move: &GameMove,
-        _game_captures: &Option<Capture>,
+        game_move: &GameMove,
+        game_captures: &Option<Capture>,
         _board: &Vec<Vec<Option<Player>>>,
-        _history_captures: &HashMap<Player, (usize, HashSet<usize>)>,
+        history_captures: &HashMap<Player, (usize, HashSet<usize>)>,
     ) -> Option<Win> {
-        {
-            info!("Checking for win:");
-            // check for 5 captures
-            let current_player = _game_move.player_id;
-            info!("Current player: {:?}", current_player);
-            // check if there is a capture of opponent
-            if let Some(capture) = &_game_captures {
-                info!("Capture found: {:?}", capture);
-                // count number of captures
-                let captures = _history_captures
-                    .get(&current_player)
-                    .map(|(count, _)| *count)
-                    .unwrap_or(0);
-                info!(
-                    "Current player {:?} has {} captures",
-                    current_player, captures
-                );
-                // counting captures including the current one
-                if captures + capture.num_captures >= 5 {
-                    info!("Player {:?} wins by captures!", current_player);
-                    return Some(Win {
-                        player_id: current_player,
-                        win_seq: None,
-                        is_by_five: false,
-                    });
-                }
-            }
+        info!("Checking for win:");
+
+        let current_player = game_move.player_id;
+        info!("Current player: {:?}", current_player);
+
+        // Check win by captures
+        if let Some(win) = game_captures.as_ref().and_then(|cap| {
+            info!("Capture found: {:?}", cap);
+            Self::check_win_by_captures(current_player, cap, history_captures)
+        }) {
+            return Some(win);
         }
+
+        // TODO: check win by five in a row
+
+        None
+    }
+
+    fn check_win_by_captures(
+        player: Player,
+        current_capture: &Capture,
+        history_captures: &HashMap<Player, (usize, HashSet<usize>)>,
+    ) -> Option<Win> {
+        let previous_captures = history_captures
+            .get(&player)
+            .map(|(count, _)| *count)
+            .unwrap_or(0);
+
+        let total_captures = previous_captures + current_capture.num_captures;
+
+        info!(
+            "Current player {:?} has {} captures",
+            player, previous_captures
+        );
+
+        if total_captures >= Self::CAPTURES_TO_WIN {
+            info!("Player {:?} wins by captures!", player);
+            return Some(Win {
+                player_id: player,
+                win_seq: None,
+                is_by_five: false,
+            });
+        }
+
         None
     }
 }
